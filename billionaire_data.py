c80e93024df5afc0f39ae9ea55f81a3ff2f65700
chamsom <jaekimiyo@gmail.com>
2019-12-18 15:45:02 -0600

    SECOND COLUMN PRINTS HOW I WANT IT TO ;) TEARS OF JOY...

--- billionaire_data.py
+++ billionaire_data.py
@@ -1,17 +1,13 @@
 import urllib
 import urllib.request
 from bs4 import BeautifulSoup
-import csv
-
 
 def make_soup(url):
     forbes = urllib.request.urlopen(url)
     soupdata = BeautifulSoup(forbes, 'html.parser')
     return soupdata
 
-
 soup = make_soup('https://www.forbes.com/profile/sheldon-adelson/?list=billionaires#d4342084a224')
-soup.get_text(strip = True)
 
 for profile in soup.findAll('div', {'class': 'profile-content'}):
 
@@ -23,9 +19,6 @@ for profile in soup.findAll('div', {'class': 'profile-content'}):
     for info in soup.findAll('div', {'class': 'profile-info'}):
         worth = soup.find('div', {'class': 'profile-info__item-value'}).text
         print('Net Worth: ' + worth)
-
-    for stats in soup.findAll('div', {'class': 'profile-stats__item'}):
-        print(stats.text)
 
 with open('Forbes.csv', 'w') as r:
     for profile in soup.findAll('div', {'class': 'profile-content'}):
@@ -41,18 +34,16 @@ with open('Forbes.csv', 'w') as r:
 
         for stats in soup.findAll('div', {'class': 'profile-stats'}):
 
-            for title in soup.findAll('span', {'class': 'profile-stats__title'}):
-                new_title = title.text
-                r.write(new_title + '\n')
+            title = soup.find_all(attrs={'class': "profile-stats__title"})
+            title_info = soup.find_all(attrs={'class': "profile-stats__text"})
 
-            # for p in soup.findAll('span', {'class': 'profile-stats__text'}): uncomment line to see why it gets a bit strange here for the second column
+            for title, title_info in zip(title, title_info): # who knew that zip would come in clutch from Dr. Putonti?
+                r.write(title.getText() + ': ' + title_info.getText() + '\n')
+
+            # for title in soup.findAll('span', {'class': 'profile-stats__title'}): DISCONTINUED LOOPS SINCE THIS BEHAVES LIKE GARBO
+            #     new_title = title.text
+            #     r.write(new_title + ': ' + '\n')
+            #
+            #
+            # for p in soup.findAll('span', {'class': 'profile-stats__text'}):
             #     new_p = p.text
-            #     r.write(new_p)
-
-
-
-
-
-
-
-            # r.write(stats.text + '\n')
