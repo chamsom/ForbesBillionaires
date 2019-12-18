6d63a9e295c03220cb2c66fa7f16121b79f0825f
chamsom <jaekimiyo@gmail.com>
2019-12-18 13:25:22 -0600

    csv working but second column is printing out incorrectly...just why...

--- billionaire_data.py
+++ billionaire_data.py
@@ -1,7 +1,7 @@
 import urllib
 import urllib.request
-import csv
 from bs4 import BeautifulSoup
+import csv
 
 
 def make_soup(url):
@@ -9,28 +9,44 @@ def make_soup(url):
     soupdata = BeautifulSoup(forbes, 'html.parser')
     return soupdata
 
+
 soup = make_soup('https://www.forbes.com/profile/sheldon-adelson/?list=billionaires#d4342084a224')
-# for stats in soup.findAll('div', {'class':'profile-stats__item'}): testing purposes ignore line
-#     print(stats.find('span').text)
+soup.get_text(strip = True)
 
-for profile in soup.findAll('div', {'class':'profile-content'}):
+for profile in soup.findAll('div', {'class': 'profile-content'}):
 
-    for name in soup.findAll('div', {'class':'profile-heading--desktop'}):
+    for name in soup.findAll('div', {'class': 'profile-heading--desktop'}):
         richnames = name.h1.text
         richowner = name.div.text
     print(richnames + ': ' + richowner)
 
-    for info in soup.findAll('div', {'class':'profile-info'}):
-        worth = soup.find('div', {'class':'profile-info__item-value'}).text
+    for info in soup.findAll('div', {'class': 'profile-info'}):
+        worth = soup.find('div', {'class': 'profile-info__item-value'}).text
         print('Net Worth: ' + worth)
 
-    for stats in soup.findAll('div', {'class':'profile-stats__item'}):
+    for stats in soup.findAll('div', {'class': 'profile-stats__item'}):
         print(stats.text)
 
-    # for profile_stats in soup.findAll('span', {'class':'profile-stats__title'}):
-    #     print(profile_stats.text)
+with open('Forbes.csv', 'w') as r:
+    for profile in soup.findAll('div', {'class': 'profile-content'}):
+
+        for name in soup.findAll('div', {'class': 'profile-heading--desktop'}):
+            richnames = name.h1.text
+            richowner = name.div.text
+        r.write(richnames + ': ' + richowner + '\n')
+
+        for info in soup.findAll('div', {'class': 'profile-info'}):
+            worth = soup.find('div', {'class': 'profile-info__item-value'}).text
+        r.write('Net Worth: ' + worth + '\n')
+
+        for stats in soup.findAll('div', {'class': 'profile-stats'}):
+
+            for title in soup.findAll('span', {'class': 'profile-stats__title'}):
+                r.write(title.text + ': ' + '\n')
+
+            # for p in soup.findAll('span', {'class': 'profile-stats__text'}):
+            #     r.write(p.text)
 
 
-    # profile_stats = soup.find('div', {'class':'profile-stats__item'}).text
-    # print(profile_stats)
 
+            # r.write(stats.text + '\n')
