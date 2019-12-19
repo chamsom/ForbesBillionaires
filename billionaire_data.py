73935dd2ed1cc50bb29dc5936b9f96eca47b8d03
chamsom <jaekimiyo@gmail.com>
2019-12-19 14:46:54 -0600

    cleaned up formatting and added links_scraped.py file to identify next links to scrape...

--- billionaire_data.py
+++ billionaire_data.py
@@ -33,16 +33,8 @@ with open('Forbes.csv', 'w') as r:
 
         for stats in soup.findAll('div', {'class': 'profile-stats'}):
 
-            title = soup.find_all(attrs={'class': "profile-stats__title"})
-            title_info = soup.find_all(attrs={'class': "profile-stats__text"})
+            title = soup.find_all(attrs = {'class': "profile-stats__title"})
+            title_info = soup.find_all(attrs = {'class': "profile-stats__text"})
 
             for title, title_info in zip(title, title_info): # who knew that zip would come in clutch from Dr. Putonti?
                 r.write(title.getText() + ',' + title_info.getText() + '\n')
-
-            # for title in soup.findAll('span', {'class': 'profile-stats__title'}): DISCONTINUED LOOPS SINCE THIS BEHAVES LIKE GARBO
-            #     new_title = title.text
-            #     r.write(new_title + ': ' + '\n')
-            #
-            #
-            # for p in soup.findAll('span', {'class': 'profile-stats__text'}):
-            #     new_p = p.text
